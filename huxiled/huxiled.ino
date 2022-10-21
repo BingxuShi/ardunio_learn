12330e436df53419a054bebaccf3c3b5396bd342
bignxushi <pizzapaigu666@gmail.com>
2022-10-22 00:17:30 +0800

    zhushi

--- huxiled/huxiled.ino
+++ huxiled/huxiled.ino
@@ -4,17 +4,18 @@ int flag = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(pin, OUTPUT);
+  pinMode(pin, OUTPUT); //设置13脚为输出模式
 
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(pin, HIGH);
-  delayMicroseconds(i);
-  digitalWrite(pin, LOW);
-  delayMicroseconds(1000 - i);
+  digitalWrite(pin, HIGH);     //把13脚设置高电瓶
+  delayMicroseconds(i);        //delay i us
+  digitalWrite(pin, LOW);      //把13脚设置低电平
+  delayMicroseconds(1000 - i); //delay 1000 - i us
+  /* 占时间逐渐增高随后逐渐降低 */
   if(flag == 0)
   {
     i++;
@@ -29,7 +30,6 @@ void loop() {
     if(i == 0)
     {
       flag = 0;
-    }
-  
+    } 
   }
 }
